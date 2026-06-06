7b00f94160da53d5a7d8d2b2ccbf1bc018619e4b
furqatahmadaliyev <furqatahmadaliyev4@gmail.com>
2026-06-06 15:50:18 +0500

    yangi ozgarishlar
    yangi o'zgarishlar bor

---
+++ python_12_dars/listga_misollar.py
@@ -0,0 +1,12 @@
+
+
+cars = ['audi', 'general motors', 'bmw', 'volvo', 'tesla']
+sonlar = [123, 24, 0, 78, 99, 147]
+new_cars = cars[:]
+new_cars.remove('audi')
+print(cars)
+print(new_cars)
+
+
+
+
